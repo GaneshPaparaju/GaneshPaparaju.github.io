644268294e7f2510761d4619572bf49c858165ea
GANESH PAPARAJU <paparaju.s@northeastern.edu>
2025-03-17 00:46:49 -0400

    Delete src directory

--- src/PortfolioHome.jsx
+++
@@ -1,8 +0,0 @@
-export default function PortfolioHome() {
-  return (
-    <div>
-      <h1>Hello, this is Ganesh's Portfolio!</h1>
-      <p>This is a working React Vite page deployed on GitHub Pages.</p>
-    </div>
-  );
-}
